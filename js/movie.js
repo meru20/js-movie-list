b498172af3f23e830d4ebb260b03a605af55bcd6
meru20 <merhawitgubsa@gmail.com>
2021-03-03 22:42:23 -0800

    feat(movie.js, movie.html): wired up the html file with js functionality

--- js/movie.js
+++ js/movie.js
@@ -1,26 +1,60 @@
 /**functions for movie list***/
 
 let movieList = [
-  {id: 1, title:'Inception', year: 2010, genre:'Adventure', descrpion:'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', imdbRating:8.8,
-},
-  {id: 2, title:'Martin', year: 1992, genre:'Comedy', descrpion:'Sassy sitcom centering on radio and television personality Martin Payne. Series focuses on his romantic relationship with girlfriend Gina, her best friend Pam and escapades with best friends Tommy and Cole.', imdbRating:7.4,
-},
-  {id: 3, title:'Fresh prince of bel-Air', year: 1990, genre:'Comedy', descrpion:'A streetwise, poor young man from Philadelphia is sent by his mother to live with his aunt, uncle and cousins in their Bel-Air mansion.', imdbRating:8.8,
-},
-  {id: 4, title:'Friends', year: 1994, genre:'Comedy', descrpion:'Follows the personal and professional lives of six twenty to thirty-something-year-old friends living in Manhattan.', imdbRating:8.9,
-},
-
-
-];
+    {
+      id : 1,
+      title:'Interstellar',
+      year:2014,
+      genre:'Science fiction',
+      description:'description',
+      imdbRating:8.6
+    },
+    {
+      id: 2,
+      title:'Martian',
+      year:2015,
+      genre:'Adventure  ',
+      description:'description',
+      imdbRating:8.0
+    },
+    {
+      id: 3,
+      title:'Avatar',
+      year:2009,
+      genre:'Action',
+      description:'description',
+      imdbRating:7.8
+    },
+    {
+    id: 4,
+    title:'Inception',
+    year:2010,
+    genre:'Action',
+    description:'description',
+    imdbRating:8.8
+    },
+    {
+    id: 5,
+    title:'The Avengers',
+    year:2012,
+    genre:'Action',
+    description:'description',
+    imdbRating:8.0
+    },
+  ]
 /*******2 a -Alert with title and description */
-const detail = movieId => {
 
-let movieTitle =  movieList.find(movie => movie. id === movieId).title
-let movieDescription =  movieList.find(movie => movie. id === movieId).descrpion;
-  alert('Movie Title :' + movieTitle +
-  'Movie Description :'+ movieDescription);
+const detailMovie = movieTitle => {
+
+let findMovie =  movieList.find(movie => movie.title === movieTitle);
+if(findMovie) {
+  alert(`${findMovie.title}:  ${findMovie.description}`);
+}
+  else {
+    console.log ('Movie not found :(')
  }
-  detail(1);
+}
+  detailMovie('The Avengers');
 
 /*************3. Create new movie object and push it */
 
@@ -46,12 +80,10 @@ addMovie(5,'The Intouchables',2011 , 'Biography', 'After he becomes a quadripleg
 
 /*************4. remove an object based on its attribute */
 
-const removeMovie = movieName => {
-  movieList = movieList.filter((movie) => {
-    return movie.title !== movieName
-  })
+const removeMovie = movieId => {
+  movieList = movieList.filter(movie => movie.id !== movieId)
   console.log('You have deleted one movie', movieList);
 
 
   }
-removeMovie('Inception');
+removeMovie(4);
